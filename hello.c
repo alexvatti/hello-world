e2ec4379474b14cd5071f04b451e3336731034d4
Alex <alexvatti@gmail.com>
2022-09-14 14:19:06 +0530

    Update hello.c
    Calling add function with fixed arguments

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+int add(int,int)
 int main()
 {
     printf("Welcome to hello world program...build!\n");
+    printf("Sum of two numbers:",add(10,15))"
+    printf("\n");
     return 0;
 }
